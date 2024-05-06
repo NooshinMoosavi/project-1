3b19fe0e772256d411f98fd876c41dbdaf5e961c
NooshinMoosavi <nooshin.moosavi.2005@gmail.com>
2024-05-07 01:31:53 +0430

    second commit

--- code-1.cpp
+++ code-1.cpp
@@ -4,8 +4,34 @@ using namespace std;
 class trim
 {
     public:
-    string trim1()
+    string trim1(const string &u)
     {
-        
+        int start = 0;
+        int end = u.length() - 1;
+        while (start <= end && (u[start] == ' ' || u[start] == '\t' || u[start] == '\n' || u[start] ==  '\r'))
+        {
+            start++;
+        }
+        while (end >= start && (u[end] == ' ' || u[end] == '\t' || u[end] == '\n' || u[end] ==  '\r'))
+        {
+            end--;
+        }
+        return u.substr(start, end-start + 1);
     }
 };
+int main()
+{
+    trim tr;
+    while(true)
+    {
+        cout<<"Welcome dear teacher !"<<endl;
+        string userName, passWord;
+        cout<<"Enter userName: ";
+        getline(cin, userName);
+        userName = tr.trim1(userName);
+        cout<<"Enter passWord: ";
+        getline(cin, passWord);
+        passWord = tr.trim1(passWord);
+    }
+    return 0;
+}
